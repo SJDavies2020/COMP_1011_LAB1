e98b9da4ace04d0b31ec570e66423a0e9f1fcdb9
SJDavies2020 <200411361@student.georgianc.on.ca>
2020-09-20 17:29:21 -0400

    Updated Print to Screen

--- COMP_1011_Lab1/src/Main.java
+++ COMP_1011_Lab1/src/Main.java
@@ -1,12 +1,25 @@
+import java.text.NumberFormat;
 import java.time.LocalDate;
+import java.util.Locale;
 
 public class Main {
     public static void main(String[] args) {
+
+        NumberFormat format = NumberFormat.getCurrencyInstance(Locale.CANADA);
+
         Game game1 = new Game("Lode Runner","2020",false, LocalDate.of(2021,5,5),"2D Platformer",49.99);
         Game game2 = new Game("Escape From Ontario","2020",false, LocalDate.of(2021,5,5),"FPS",89.99);
 
         System.out.printf("Game 1: %s%n", game1.toString());
-        System.out.printf("Game 2: %s%n", game2.toString());
-
+        System.out.printf("Game 1: Is Published?" + game1.isPublished() + " \n") ;
+        System.out.printf("Game 1: Publish Year: " + game1.getPublishYear().toString() + " \n") ;
+        System.out.printf("Game 1: Type: " + game1.getGameType().toString() + " \n") ;
+        System.out.printf("Game 1: Price: " + format.format(game1.getGamePrice()) + " \n");
+        System.out.printf(" \n") ;
+        System.out.printf("Game 2: %s%n", game1.toString());
+        System.out.printf("Game 2: Is Published?" + game1.isPublished() + " \n") ;
+        System.out.printf("Game 2: Publish Year: " + game1.getPublishYear().toString() + " \n") ;
+        System.out.printf("Game 2: Type: " + game1.getGameType().toString() + " \n") ;
+        System.out.printf("Game 2: Price: " + format.format(game1.getGamePrice()) + " \n");
     }
 }
